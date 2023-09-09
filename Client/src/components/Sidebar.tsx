860ccbefb16044dc0be15be680fd679eed7ea1f0
BipulLamsal <bipullamsal@gmail.com>
2023-09-09 09:01:53 +0545

    active tabs

--- Client/src/components/Sidebar.tsx
+++ Client/src/components/Sidebar.tsx
@@ -1,47 +1,64 @@
 import { MdExplore, MdQuiz } from "react-icons/md";
 import { PiNoteFill } from "react-icons/pi";
-import { CgMediaLive } from "react-icons/cg";
+import { CgLivePhoto } from "react-icons/cg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 function Sidebar() {
-    // const sideBarNavItems = [
-    //     {
-    //         display: 'Feed',
-    //         icon : <MdExplore></MdExplore>,
-    //         to:'/'
+  const [isActive, setIsActive] = useState(0);
+  const handleOnClick = (index: number) => {
+    setIsActive(index);
+  };
 
-    //     }
-    // ]
+  const sideBarNavItems = [
+    {
+      id: 0,
+      display: "Feed",
+      icon: <MdExplore className="w-1/8 text-2xl"></MdExplore>,
+      to: "/",
+    },
+    {
+      id: 1,
+      display: "Quiz",
+      icon: <MdQuiz className="w-1/8 text-2xl"></MdQuiz>,
+      to: "/quiz",
+    },
+    {
+      id: 2,
+      display: "Flash Card",
+      icon: <PiNoteFill className="w-1/8 text-2xl"></PiNoteFill>,
+      to: "/flashcard",
+    },
+    {
+      id: 3,
+      display: "Quiz Hunt",
+      icon: <CgLivePhoto className="w-1/8 text-2xl"></CgLivePhoto>,
+      to: "/quizhunt",
+    },
+  ];
   return (
     <div className="bg-purple-500 text-white w-1/4 h-screen px-12 py-2">
       <ul className="mt-4 flex flex-col">
-        <li className="mb-2 py-3 px-5 text-purple-100 hover:bg-purple-800 rounded-md hover:text-white">
-          <a href="#" className="flex flex-row items-center gap-5">
-            <p>
-              <MdExplore className="w-1/8 text-2xl"></MdExplore>
-            </p>
-            Feed
-          </a>
-        </li>
-        <li className="mb-2 py-3 px-5 text-purple-100 hover:bg-purple-800 rounded-md hover:text-white">
-          <a href="#" className="flex flex-row items-center gap-5">
-            <p>
-              <MdQuiz className="w-1/8 text-2xl"></MdQuiz>
-            </p>
-            Quiz
-          </a>
-        </li>
-        <li className="mb-2 py-3 px-5 text-purple-100 hover:bg-purple-800 rounded-md hover:text-white">
-          <a href="#" className="flex flex-row items-center gap-5">
-            <PiNoteFill className="w-1/8 text-2xl"></PiNoteFill>
-            Flash Card
-          </a>
-        </li>
-        <li className="mb-2 py-3 px-5 text-purple-100 hover:bg-purple-800 rounded-md hover:text-white">
-          <a href="#" className="flex items-center gap-5">
-            <CgMediaLive className="w-1/8 text-2xl" />
-
-            <div className="">Quiz Hunt</div>
-          </a>
-        </li>
+        {sideBarNavItems.map((item) => {
+          return (
+            <Link
+              to={item.to}
+              onClick={() => {
+                handleOnClick(item.id);
+              }}
+            >
+              <li
+                className={`mb-2 py-3 px-5 text-purple-100 hover:bg-purple-800 rounded-md hover:text-white ${
+                  isActive === item.id && `bg-purple-800 text-white`
+                }`}
+              >
+                <p className="flex flex-row items-center gap-5">
+                  <p>{item.icon}</p>
+                  {item.display}
+                </p>
+              </li>
+            </Link>
+          );
+        })}
       </ul>
     </div>
   );
